581bbdce655864c6126edc9002c68f7a03e03bba
Cheng-Yi (Charlie) Li <100334852+charlierabea@users.noreply.github.com>
2025-02-12 15:15:34 -0500

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -3,7 +3,7 @@ export PYTHONPATH="your PYTHON PATH"
 #This code capture the report output during the optimization process and record the report to an excel file (which should be specified in /pipeline/train/eval.py
 CUDA_VISIBLE_DEVICES=1 accelerate launch --config_file=./evaluation/pipeline/accelerate_configs/accelerate_config_fsdp.yaml \
 ./evaluation/pipeline/train/eval.py \
---pretrained_model_name_or_path="./checkpoints/OTTER_CLIP_RADICAL_hf/" \
+--pretrained_model_name_or_path="./checkpoints/OTTER_CLIP_BRAINGPT_hf/" \
 --mimicit_path="./data/CQ500p_instruction.json" \
 --images_path="./data/CQ500p.json" \
 --batch_size=1 \
