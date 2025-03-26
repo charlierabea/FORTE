4546e72ab8c0b7472428c574471e921008a55f1b
Matteo Bellitti <matteo.bellitti@gmail.com>
2025-03-26 11:31:14 -0400

    Update eval.sh
    Clarified why `PYTHONPATH` is necessary

--- eval.sh
+++ eval.sh
@@ -1,3 +1,5 @@
+export PYHTONPATH="./evaluation/" # Change this path to your local path of FORTE/evaluation/ directory, where python will look for additional module. Necessary for correctly importing modules from evaluation/ 
+
 #This code capture the report output during the optimization process and record the report to an excel file (which should be specified in /pipeline/train/eval.py
 accelerate launch --config_file=./evaluation/pipeline/accelerate_configs/accelerate_config_fsdp.yaml \
 ./evaluation/pipeline/train/eval.py \
